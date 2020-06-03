25f2c2e6eed1fff7512ba9109a106563681c15f3
Diego Arias <diego.arias@yuju.io>
2020-06-03 15:36:38 -0500

    Converted values to float
    TODO: - Homologate names
          - Get better USD buy and USD sell

--- currency_uy/currency_uy/pipelines.py
+++ currency_uy/currency_uy/pipelines.py
@@ -13,7 +13,12 @@ class CurrencyUyPipeline:
 
     def process_item(self, item, spider):
         exchange_house = self.result.setdefault(spider.name, [])
-        exchange_house.append(item.__dict__['_values'])
+
+        values = item.__dict__['_values']
+        values['buy'] = round(float(values['buy'].replace(',', '.')), 2)
+        values['sell'] = round(float(values['sell'].replace(',', '.')), 2)
+
+        exchange_house.append(values)
         return item
 
     def close_spider(self, spider):
